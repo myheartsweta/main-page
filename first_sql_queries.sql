180bf621641ac9d0bc67b05161846241bcf03968
_SHWETA_SINGH_ <154076280+myheartsweta@users.noreply.github.com>
2025-03-02 00:03:07 +0530

    Update first_sql_queries.sql

--- first_sql_queries.sql
+++ first_sql_queries.sql
@@ -1,13 +1,16 @@
 CREATE DATABASE sales_data;
 USE sales_data;
 SELECT * FROM retail_sales_dataset LIMIT 10;
+
 ##count total rows#
 select count(*)from_sales_dataset limit 20;
+
 ##check for null values
 select age, count(*)as null_count
 from retail_sales_dataset
 where age is null
 group by age;
+
 ##total sales revenue
 SELECT Product_Category, SUM(Quantity) AS Total_Sold
 FROM retail_sales_dataset
@@ -19,15 +22,20 @@ SELECT * FROM retail_sales_dataset LIMIT 10;
 
 #count the number of rfecords
 select count(*) from retail_sales_dataset;
+
 #data filtering
 select * from retail_sales_dataset where Quantity = 4;
+
 #get transaction from a spesinfic date range
 select * from retail_sales_dataset where Date between '2023-01-01' and '2023-10-01';
+
 #aggregation grouping
 #total revenue generated
 select sum(Total_Amount) as total_revenue from retail_sales_dataset;
+
 #revenue generated per product
 select Product_Category, sum(Total_Amount) as revenue from retail_sales_dataset group by Product_Category;
+
 #average sales gender wise
 select Gender, avg(Total_Amount) as avg_amnt from retail_sales_dataset group by Gender;
 
